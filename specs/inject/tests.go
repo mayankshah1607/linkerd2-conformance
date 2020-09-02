a59b93d4b8f4950b2a880c69bacdfc8a740b911d
Mayank Shah <mayankshah1614@gmail.com>
2020-09-02 13:40:12 +0530

    Clean up lifecycle and inject tests
    - Add line breaks where lines get too long
    - Remove unnecessary `ginkgo.By` log messages
    
    Signed-off-by: Mayank Shah <mayankshah1614@gmail.com>

--- specs/inject/tests.go
+++ specs/inject/tests.go
@@ -7,7 +7,6 @@ import (
 	"github.com/linkerd/linkerd2-conformance/utils"
 	"github.com/linkerd/linkerd2/pkg/k8s"
 	"github.com/linkerd/linkerd2/testutil"
-	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
@@ -34,7 +33,6 @@ func testInjectManual(withParams bool) {
 	}
 
 	if withParams {
-		ginkgo.By("Adding manual parameters to `linkerd inject`")
 		params := []string{
 			"--disable-tap",
 			"--image-pull-policy=Never",
@@ -61,10 +59,10 @@ func testInjectManual(withParams bool) {
 	// }
 	cmd = append(cmd, injectYAMLPath)
 
-	ginkgo.By(fmt.Sprintf("Running `linkerd inject` against %s", injectYAMLPath))
 	out, stderr, err := h.LinkerdRun(cmd...)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to run `linkerd inject`: %s\n%s", out, stderr))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to run `linkerd inject`: %s\n%s", out, stderr))
 
 	// This part is flaky and is being temporarily omitted.
 	// The output of `linkerd inject --manual` may differ
@@ -78,7 +76,6 @@ func testInjectManual(withParams bool) {
 func testProxyInjection() {
 	h, _ := utils.GetHelperAndConfig()
 
-	ginkgo.By("Reading pod YAML")
 	podYAML, err := testutil.ReadFile("testdata/inject/pod.yaml")
 
 	gomega.Expect(err).Should(gomega.BeNil(), utils.Err(err))
@@ -90,22 +87,25 @@ func testProxyInjection() {
 	}
 
 	proxyInjectTestNs = h.GetTestNamespace(injectNs)
-	ginkgo.By(fmt.Sprintf("Creating data plane namespace %s", proxyInjectTestNs))
 	err = h.CreateDataPlaneNamespaceIfNotExists(proxyInjectTestNs, nsAnnotations)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to create namespace %s: %s", proxyInjectTestNs, utils.Err(err)))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to create namespace %s: %s", proxyInjectTestNs, utils.Err(err)))
 
-	ginkgo.By(fmt.Sprintf("Creating test pod in namespace %s", proxyInjectTestNs))
 	o, err := h.Kubectl(podYAML, "-n", proxyInjectTestNs, "create", "-f", "-")
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to create pod/%s in namespace %s for %s: %s", podName, proxyInjectTestNs, utils.Err(err), o))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to create pod/%s in namespace %s for %s: %s",
+			podName, proxyInjectTestNs, utils.Err(err), o))
 
-	ginkgo.By("Waiting for pod to be initialized")
-	o, err = h.Kubectl("", "-n", proxyInjectTestNs, "wait", "--for=condition=initialized", "--timeout=120s", "pod/"+podName)
+	o, err = h.Kubectl("", "-n", proxyInjectTestNs, "wait",
+		"--for=condition=initialized",
+		"--timeout=120s", "pod/"+podName)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to wait for pod/%s to be initialized in namespace %s: %s: %s", podName, proxyInjectTestNs, utils.Err(err), o))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to wait for pod/%s to be initialized in namespace %s: %s: %s",
+			podName, proxyInjectTestNs, utils.Err(err), o))
 
-	ginkgo.By("Checking for existence of proxy container")
 	err = h.RetryFor(time.Minute*3, func() error {
 		pods, err := h.GetPods(proxyInjectTestNs, map[string]string{"app": podName})
 		if err != nil {
@@ -127,7 +127,6 @@ func testInjectAutoNsOverrideAnnotations() {
 
 	h, _ := utils.GetHelperAndConfig()
 
-	ginkgo.By("Reading pod YAML")
 	injectYAML, err := testutil.ReadFile("testdata/inject/inject_test.yaml")
 
 	gomega.Expect(err).Should(gomega.BeNil(), utils.Err(err))
@@ -145,42 +144,50 @@ func testInjectAutoNsOverrideAnnotations() {
 
 	nsAnnotationsOverrideTestNs = h.GetTestNamespace(injectNs)
 
-	ginkgo.By(fmt.Sprintf("Creating data plane namespace %s", proxyInjectTestNs))
 	err = h.CreateDataPlaneNamespaceIfNotExists(nsAnnotationsOverrideTestNs, nsAnnotations)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to create namespace %s: %s", nsAnnotationsOverrideTestNs, utils.Err(err)))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to create namespace %s: %s",
+			nsAnnotationsOverrideTestNs, utils.Err(err)))
 
 	podProxyCPUReq := "600m"
 	podAnnotations := map[string]string{
 		k8s.ProxyCPURequestAnnotation: podProxyCPUReq,
 	}
 
-	ginkgo.By("Patching inject test YAML")
 	patchedYAML, err := testutil.PatchDeploy(injectYAML, deployName, podAnnotations)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to patch inject test YAML in namespace %s for deploy/%s: %s", nsAnnotationsOverrideTestNs, deployName, utils.Err(err)))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to patch inject test YAML in namespace %s for deploy/%s: %s",
+			nsAnnotationsOverrideTestNs, deployName, utils.Err(err)))
 
-	ginkgo.By("Applying patched YAML to your cluster")
 	o, err := h.Kubectl(patchedYAML, "-n", nsAnnotationsOverrideTestNs, "create", "-f", "-")
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to create deploy/%s in namespace %s for  %s: %s", deployName, nsAnnotationsOverrideTestNs, utils.Err(err), o))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to create deploy/%s in namespace %s for  %s: %s",
+			deployName, nsAnnotationsOverrideTestNs, utils.Err(err), o))
 
-	ginkgo.By(fmt.Sprintf("Waiting for deploy/%s to be available", deployName))
-	o, err = h.Kubectl("", "--namespace", nsAnnotationsOverrideTestNs, "wait", "--for=condition=available", "--timeout=120s", "deploy/"+deployName)
+	o, err = h.Kubectl("", "--namespace", nsAnnotationsOverrideTestNs,
+		"wait", "--for=condition=available", "--timeout=120s", "deploy/"+deployName)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to wait for deploy/%s in namespace %s for  %s: %s", deployName, nsAnnotationsOverrideTestNs, utils.Err(err), o))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to wait for deploy/%s in namespace %s for  %s: %s",
+			deployName, nsAnnotationsOverrideTestNs, utils.Err(err), o))
 
-	ginkgo.By(fmt.Sprintf("Getting pods for deploy/%s in namespace %s", deployName, nsAnnotationsOverrideTestNs))
 	pods, err := h.GetPodsForDeployment(nsAnnotationsOverrideTestNs, deployName)
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to get pods for namespace %s: %s", nsAnnotationsOverrideTestNs, utils.Err(err)))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to get pods for namespace %s: %s",
+			nsAnnotationsOverrideTestNs, utils.Err(err)))
 
 	containers := pods[0].Spec.Containers
 	proxyContainer := testutil.GetProxyContainer(containers)
 
-	ginkgo.By("Matching pod configuration with namespace level overrides")
-	gomega.Expect(proxyContainer.Resources.Requests["memory"]).Should(gomega.Equal(resource.MustParse(nsProxyMemReq)), "proxy memory resource request failed to match with namespace level override")
-	gomega.Expect(proxyContainer.Resources.Requests["cpu"]).Should(gomega.Equal(resource.MustParse(podProxyCPUReq)), "proxy cpu resource request failed to match with namespace level override")
+	gomega.Expect(proxyContainer.Resources.Requests["memory"]).Should(gomega.Equal(resource.MustParse(nsProxyMemReq)),
+		"proxy memory resource request failed to match with namespace level override")
+
+	gomega.Expect(proxyContainer.Resources.Requests["cpu"]).Should(gomega.Equal(resource.MustParse(podProxyCPUReq)),
+		"proxy cpu resource request failed to match with namespace level override")
 }
 
 func testClean() {
@@ -192,18 +199,18 @@ func testClean() {
 	}
 
 	for _, ns := range namespaces {
-		ginkgo.By(fmt.Sprintf("Gathering manifests for namespace/%s", ns))
 		out, err := h.Kubectl("", "-n", ns, "get", "all", "-o", "yaml")
 
 		gomega.Expect(err).Should(gomega.BeNil(), utils.Err(err))
 
-		ginkgo.By(fmt.Sprintf("Deleting resources in namespace/%s", ns))
 		_, err = h.Kubectl(out, "delete", "-f", "-")
 
-		gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("could not delete resources in namespace/%s: %s", ns, utils.Err(err)))
+		gomega.Expect(err).Should(gomega.BeNil(),
+			fmt.Sprintf("could not delete resources in namespace/%s: %s", ns, utils.Err(err)))
 
 		_, err = h.Kubectl("", "delete", "ns", ns)
 
-		gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("could not delete namespace %s: %s", ns, utils.Err(err)))
+		gomega.Expect(err).Should(gomega.BeNil(),
+			fmt.Sprintf("could not delete namespace %s: %s", ns, utils.Err(err)))
 	}
 }
--- specs/lifecycle/tests.go
+++ specs/lifecycle/tests.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 
 	"github.com/linkerd/linkerd2-conformance/utils"
-	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 )
 
 func testUpgradeCLI() {
 	h, c := utils.GetHelperAndConfig()
-
-	ginkgo.By(fmt.Sprintf("Upgrading CLI from version %s to %s", h.UpgradeFromVersion(), h.GetVersion()))
 
 	err := utils.InstallLinkerdBinary(c.GetLinkerdPath(), h.GetVersion(), true, false)
 	gomega.Expect(err).Should(gomega.BeNil(), utils.Err(err))
@@ -22,10 +19,12 @@ func testUpgradeCLI() {
 		"--client",
 	}
 
-	ginkgo.By("Validating CLI version")
 	out, stderr, err := h.LinkerdRun(cmd...)
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("could not run `linkerd version command`: %s", stderr))
-	gomega.Expect(out).Should(gomega.ContainSubstring(h.GetVersion()), "failed to upgrade CLI")
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("could not run `linkerd version command`: %s", stderr))
+
+	gomega.Expect(out).Should(gomega.ContainSubstring(h.GetVersion()),
+		"failed to upgrade CLI")
 }
 
 func testUpgrade() {
@@ -33,13 +32,14 @@ func testUpgrade() {
 
 	cmd := "upgrade"
 
-	ginkgo.By("Running `linkerd upgrade` command")
 	out, stderr, err := h.LinkerdRun(cmd)
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("`linkerd upgrade` command failed: %s", stderr))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("`linkerd upgrade` command failed: %s", stderr))
 
 	_, err = h.Kubectl(out, "apply", "--prune", "-l", "linkerd.io/control-plane-ns="+h.GetLinkerdNamespace(), "-f", "-")
 
-	gomega.Expect(err).Should(gomega.BeNil(), fmt.Sprintf("failed to apply manifests: %s", utils.Err(err)))
+	gomega.Expect(err).Should(gomega.BeNil(),
+		fmt.Sprintf("failed to apply manifests: %s", utils.Err(err)))
 
 	utils.TestControlPlanePostInstall(h)
 	utils.RunCheck(h, false)
